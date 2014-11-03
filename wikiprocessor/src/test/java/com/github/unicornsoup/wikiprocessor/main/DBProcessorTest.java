7e01dfb0f6663b256838b295109a2239777c4691
Anuj Agarwal <anujagarwal464@gmail.com>
2014-11-03 17:20:17 +0530

    Updated tests

--- wikiprocessor/src/test/java/com/github/unicornsoup/wikiprocessor/main/DBProcessorTest.java
+++ wikiprocessor/src/test/java/com/github/unicornsoup/wikiprocessor/main/DBProcessorTest.java
@@ -1,21 +1,22 @@
 package com.github.unicornsoup.wikiprocessor.main;
-import org.junit.Assert;
 
+import org.junit.Assert;
 import org.junit.Test;
+
 public class DBProcessorTest {
 	
 	@Test
 	public void testExtractPageIds(){
-		String categoryName = ""; 
-		String outputFile = "";
+		String categoryName = "Unprintworthy_redirects"; 
+		String outputFile = "tmpIds";
 		DBProcessor dbProcessor = new DBProcessor();
 		Assert.assertEquals(true, dbProcessor.extractPageIds(categoryName, outputFile));
 	}
 	
 	@Test
 	public void testExtractPageTitles(){
-		String inputFile = ""; 
-		String outputFile = "";
+		String inputFile = "tmpIds"; 
+		String outputFile = "tmpTitles";
 		DBProcessor dbProcessor = new DBProcessor();
 		Assert.assertEquals(true, dbProcessor.extractPageTitles(inputFile, outputFile));
 	}	
