eb7cb0bc920a06d38973f28dc4a12753264fa2c9
Anuj Agarwal <anujagarwal464@gmail.com>
2014-11-03 17:35:54 +0530

    Fixed indendation

--- wikiprocessor/src/main/java/com/github/unicornsoup/wikiprocessor/main/DBProcessor.java
+++ wikiprocessor/src/main/java/com/github/unicornsoup/wikiprocessor/main/DBProcessor.java
@@ -13,102 +13,101 @@ import java.util.Scanner;
 
 /**
  * 
- * @author sjth, anujagarwal464
+ * @author sp, anujagarwal464
  * 
- * This class extracts data from  Wikipedia database tables
- *
+ *         This class extracts data from Wikipedia database tables
+ * 
  */
 public class DBProcessor {
 	/**
-	 * This method retrieves pageIds for a specific category
-	 * from categorylinks table. It writes the results to a local file.
+	 * This method retrieves pageIds for a specific category from categorylinks
+	 * table. It writes the results to a local file.
 	 * 
 	 * @return true for success, false for failure
+	 * @throws FileNotFoundException
+	 * @throws SQLException
 	 */
-	public boolean extractPageIds(String categoryName, String outputFile){
-		
-		//! Change port accordingly
+	public boolean extractPageIds(String categoryName, String outputFile) {
+
+		// ! Change port accordingly
 		String url = "jdbc:mysql://localhost:3306/wikidb";
 		Connection con = null;
-	    Statement stmt = null;
-	    ArrayList<String> pageIds = new ArrayList<String>();
-	    
-	    try
-	    {
-	      Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-	      
-	      //! Change password accordingly
-	      con = DriverManager.getConnection(url, "root", "");
-	      stmt = con.createStatement();
-	    }
-	    catch (Exception ex)
-	    {
-	      System.out.println("Cannot open database -- make sure ODBC is configured properly.");
-	      return false;
-	    }
-	    
-	    String query = "select cl_from from categorylinks where cl_to=\"" + categoryName + '\"';
+		Statement stmt = null;
+		ArrayList<String> pageIds = new ArrayList<String>();
+
+		try {
+			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+
+			// ! Change password accordingly
+			con = DriverManager.getConnection(url, "root", "");
+			stmt = con.createStatement();
+		} catch (Exception ex) {
+			System.out
+					.println("Cannot open database -- make sure ODBC is configured properly.");
+			return false;
+		}
+
+		String query = "select cl_from from categorylinks where cl_to=\""
+				+ categoryName + '\"';
 
 		try {
 			ResultSet resultSet = stmt.executeQuery(query);
-		    while (resultSet.next()) {
-		    	pageIds.add(resultSet.getString("cl_from"));
-		    }
+			while (resultSet.next()) {
+				pageIds.add(resultSet.getString("cl_from"));
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return false;
 		}
-	    
-	    PrintWriter out;
+
+		PrintWriter out;
 		try {
 			out = new PrintWriter(outputFile);
-		    for(String pageId : pageIds) {
-		    	out.println(pageId);
-		    }
+			for (String pageId : pageIds) {
+				out.println(pageId);
+			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 			return false;
 		}
 		out.close();
-	    		
+
 		return true;
 	}
-	
+
 	/**
-	 * This method retrieves page titles for a set of page ids from the page table.
-	 * The file which contains a list of page ids is given as input, and output is
-	 * where the list of page ids and page titles are written in the following format:
-	 * pageid=pagetitle. e.g.:
-	 * 10324=Jimmy_Wales
-	 *  @return true for success, false for failure
+	 * This method retrieves page titles for a set of page ids from the page
+	 * table. The file which contains a list of page ids is given as input, and
+	 * output is where the list of page ids and page titles are written in the
+	 * following format: pageid=pagetitle. e.g.: 10324=Jimmy_Wales
+	 * 
+	 * @return true for success, false for failure
 	 */
 	public boolean extractPageTitles(String inputFile, String outputFile) {
 
-		//! Change port accordingly
+		// ! Change port accordingly
 		String url = "jdbc:mysql://localhost:3306/wikidb";
 		Connection con = null;
-	    Statement stmt = null;
-	    ArrayList<String> pageIds = new ArrayList<String>();
-	    ArrayList<String> pageTitles = new ArrayList<String>();
-	    
-	    try
-	    {
-	      Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-	      
-	      //! Change password accordingly
-	      con = DriverManager.getConnection(url, "root", "");
-	      stmt = con.createStatement();
-	    }
-	    catch (Exception ex)
-	    {
-	      System.out.println("Cannot open database -- make sure ODBC is configured properly.");
-	      System.exit(1);
-	    }
-	    
-	    Scanner scanner;
+		Statement stmt = null;
+		ArrayList<String> pageIds = new ArrayList<String>();
+		ArrayList<String> pageTitles = new ArrayList<String>();
+
+		try {
+			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+
+			// ! Change password accordingly
+			con = DriverManager.getConnection(url, "root", "");
+			stmt = con.createStatement();
+		} catch (Exception ex) {
+			System.out
+					.println("Cannot open database -- make sure ODBC is configured properly.");
+			return false;
+		}
+
+		Scanner scanner;
 		try {
 			scanner = new Scanner(new File(inputFile));
-			while (scanner.hasNext()){
+			while (scanner.hasNext()) {
 				pageIds.add(scanner.next());
 			}
 		} catch (FileNotFoundException e) {
@@ -116,32 +115,33 @@ public class DBProcessor {
 			return false;
 		}
 		scanner.close();
-		
+
 		try {
-			for(String pageId : pageIds){
-				String query = "select page_title from page where page_id=\"" + pageId + '\"';
+			for (String pageId : pageIds) {
+				String query = "select page_title from page where page_id=\""
+						+ pageId + '\"';
 				ResultSet resultSet = stmt.executeQuery(query);
 				while (resultSet.next()) {
-			    	pageTitles.add(resultSet.getString("page_title"));
-			    }
+					pageTitles.add(resultSet.getString("page_title"));
+				}
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return false;
 		}
-		
+
 		PrintWriter out;
 		try {
 			out = new PrintWriter(outputFile);
-		    for(String pageTitle : pageTitles) {
-		    	out.write(pageTitle);
-		    }
+			for (String pageTitle : pageTitles) {
+				out.write(pageTitle);
+			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 			return false;
 		}
 		out.close();
-		
+
 		return true;
 	}
 
